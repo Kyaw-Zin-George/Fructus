4371689ac1109f23f7c6193b0bc5402bf8f98eda
Kyaw-Zin-George <ktz1695@gmail.com>
2025-02-24 15:18:47 +0000

    Adding FruitCardView into the onboarding screen

---
+++ Fructus/Screens/OnboardingView.swift
@@ -0,0 +1,25 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Kyaw Thant Zin(George) on 2/24/25.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    //MARK: PROPERTIES
+    
+    //MARK: BODY
+    var body: some View {
+        TabView {
+            FruitCardView()
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+//MARK: PREVIEW
+#Preview {
+    OnboardingView()
+}
