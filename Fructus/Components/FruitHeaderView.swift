28f1d20134ccef971ccce9a97653f3b8afe504b3
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-03 19:27:36 +0000

    Customizing the header section of detail view with image

--- Fructus/Components/FruitHeaderView.swift
+++ Fructus/Components/FruitHeaderView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct FruitHeaderView: View {
+    //MARK: PROPERTIES
+    var fruit: Fruit
+    @State private var isAnimatingImage: Bool = false
+    //MARK: BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom)
+            Image(fruit.image)
+                .renderingMode(.original)
+                .resizable()
+                .scaledToFit()
+                .shadow(color: Color(red: 0, green: 0, blue: 0,opacity: 0.15), radius: 8, x: 6, y: 8)
+                .padding(.vertical, 20)
+                .scaleEffect(isAnimatingImage ? 1.0 : 0.6)
+        }
+        .onAppear(){
+            withAnimation(.easeOut(duration: 0.5)){
+                isAnimatingImage = true
+            }
+        }
     }
 }
 
 #Preview {
-    FruitHeaderView()
+    FruitHeaderView(fruit: fruitsData[0])
 }
