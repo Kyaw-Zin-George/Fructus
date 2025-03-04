04d79f7828f2912054550db5784a43564f2c5baf
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-04 18:19:28 +0000

    Settingview Part 1 - Adding Logo and General info about fruits

--- Fructus/Screens/SettingsView.swift
+++ Fructus/Screens/SettingsView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct SettingsView: View {
+    //MARK: PROPERTIES
+    @Environment(\.dismiss) var dismiss
+    //MARK: BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack (spacing: 20){
+                    //MARK: SECTION 1
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+                        
+                    ){
+                        Divider().padding(.vertical, 4)
+                        HStack(alignment: .center,spacing: 10) {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40, height: 40)
+                                .clipShape(RoundedRectangle(cornerRadius: 9))
+                            Text("Most of the fruits are low in calories so it is safe to consume them regularly.")
+                                .font(.footnote)
+                        }
+                    }
+                    
+                    //MARK: SECTION 2
+                    
+                    //MARK: SECTION 3
+                    
+                }
+                .navigationBarTitle(Text("Settings"), displayMode: .large)
+                .toolbar(){
+                    Button(action:{dismiss.callAsFunction()}) {
+                        Image(systemName: "xmark")
+                    }
+                }
+                .padding()
+            }
+        }
     }
 }
 
